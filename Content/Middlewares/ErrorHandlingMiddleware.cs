c4a75d602bf4b335bc572b81d9128bfbf5a954fe
Jaxel Rojas <jaxelr@gmail.com>
2023-07-11 10:18:04 -0400

    refactor: simplified logic for handling of exceptions

--- Content/Middlewares/ErrorHandlingMiddleware.cs
+++ Content/Middlewares/ErrorHandlingMiddleware.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -32,24 +31,24 @@ public class ErrorHandlingMiddleware
 
     private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
     {
-        var status = exception switch
+        int status = exception switch
         {
-            NotFoundException _ => HttpStatusCode.NotFound,
-            BadRequestException _ => HttpStatusCode.BadRequest,
-            NotImplementedException _ => HttpStatusCode.NotImplemented,
-            _ => HttpStatusCode.InternalServerError,
+            NotFoundException _ => StatusCodes.Status404NotFound,
+            BadRequestException _ => StatusCodes.Status400BadRequest,
+            NotImplementedException _ => StatusCodes.Status501NotImplemented,
+            _ => StatusCodes.Status500InternalServerError,
         };
 
         var problems = new ProblemDetails
         {
             Title = status.ToString(),
-            Status = (int) status,
+            Status = status,
             Detail = exception.Message
         };
 
         string result = JsonSerializer.Serialize(problems);
         context.Response.ContentType = ContentTypeProblemJson;
-        context.Response.StatusCode = (int) status;
+        context.Response.StatusCode = status;
         await context.Response.WriteAsync(result);
         return;
     }
