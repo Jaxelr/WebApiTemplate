bf5663dd527edbfc75a05c37e1559a68a9ef2fb5
Jaxel Rojas <jaxelr@gmail.com>
2025-02-23 10:15:22 -0400

    refactor: remove pragma warning in favor of editorconfig

--- Content/Repositories/Repository.cs
+++ Content/Repositories/Repository.cs
@@ -4,9 +4,7 @@ namespace WebApiTemplate.Repositories;
 
 public class Repository(DbConnection connection) : IRepository
 {
-#pragma warning disable RCS1213 // Remove unused member declaration.
     private readonly DbConnection connection = connection;
-#pragma warning restore RCS1213 // Remove unused member declaration.
 
     public string MakeHello(string name) => $"Hello world: {name}";
 }
