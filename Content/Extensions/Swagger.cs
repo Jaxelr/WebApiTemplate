bd803d7fe0729e92d7afd3cb84a95dcb9ea19ffa
Jaxel Rojas <jaxelr@gmail.com>
2025-12-15 14:09:08 -0400

    chore: upgrade to dotnet 10

--- Content/Extensions/Swagger.cs
+++ Content/Extensions/Swagger.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using WebApiTemplate.Models;
 
 namespace WebApiTemplate.Extensions;
